3a69ef215838ded1888fb071ea43746ad6a1d46d
Riessie <rjkoenraats@gmail.com>
2025-04-28 17:12:48 +0200

    main.py step 1 completed

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from turtle import Turtle, Screen
 import random
 
-# Create a turtle object
-snake = Turtle()
-snake.shape("square")
-snake.color("white")
-snake.shapesize(stretch_wid=0.5, stretch_len=0.5)
-snake.penup()
-snake.speed(0)
-
 # Create a screen object
 screen = Screen()
+screen.setup(width=600, height=600)
+# Set the background color and title
 screen.bgcolor("black")
 screen.title("Snake Game")
+
+# Create a turtle object
+snake_seg1 = Turtle("square")
+snake_seg1.color("white")
+snake_seg1.goto(0, 0)
+
+snake_seg2 = Turtle("square")
+snake_seg2.color("white")
+snake_seg2.goto(-20, 0)
+
+snake_seg3 = Turtle("square")
+snake_seg3.color("white")
+snake_seg3.goto(-40, 0)
+
 screen.exitonclick()
+
+
