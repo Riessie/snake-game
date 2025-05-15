a61c9f33edc5185ff036a86e9439948df2f3dfd2
Riessie <rjkoenraats@gmail.com>
2025-05-15 11:45:24 +0200

    snake.py added

---
+++ snake.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for i in range(3):
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(-20 * i, 0)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for i in range(len(self.segments) - 1, 0, -1):
+            x = self.segments[i - 1].xcor()
+            y = self.segments[i - 1].ycor()
+            self.segments[i].goto(x, y)
+        self.head.forward(20)
