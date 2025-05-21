d7e9d6c80128479aa3552a7baa3f0f6ba9be1b46
Riessie <rjkoenraats@gmail.com>
2025-05-21 13:42:34 +0200

    keybindings added

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ screen.title("Snake Game")
 screen.tracer(0)  # Turn off the screen updates
 
 snake = Snake()
+
+screen.listen()  # Listen for keyboard input
+screen.onkey(snake.move, "Up")  # Move the snake up when 'Up' key is pressed
+screen.onkey(snake.move, "Down")  # Move the snake down when 'Down' key is pressed
+screen.onkey(snake.move, "Left")  # Move the snake left when 'Left' key is pressed
+screen.onkey(snake.move, "Right")  # Move the snake right when 'Right' key is pressed
 # loop to move the snake
 game_on = True
 while game_on:
