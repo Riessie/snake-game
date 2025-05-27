dbe0005771c9d5987ef317c562bb8156ade4453f
Riessie <rjkoenraats@gmail.com>
2025-05-27 13:36:18 +0200

    functionality added

--- food.py
+++ food.py
@@ -5,7 +5,7 @@ class Food(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5) # Standard size is 20x20, so 0.5 makes it 10x10
         self.penup()
         self.speed("fastest")
         self.color("blue")
