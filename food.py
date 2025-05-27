e87f785f853bbab6f927eb4e00dd4a252a231c12
Riessie <rjkoenraats@gmail.com>
2025-05-27 13:32:50 +0200

    food.py added

---
+++ food.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.penup()
+        self.speed("fastest")
+        self.color("blue")
